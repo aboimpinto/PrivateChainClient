0f8efee56cabcfcdc94747377519b412a578a875
Paulo Aboim Pinto <aboimpinto@gmail.com>
2023-09-21 21:41:26 +0200

    remove Encrypt and Signining Managers

--- PrivateChainClient/Program.cs
+++ PrivateChainClient/Program.cs
@@ -8,43 +8,60 @@ public class Program
     public static void Main()
     {
         // Bitcoin Example
-        var bitcoinActorAKeys = new SigningKeys();
-        Console.WriteLine("Client A's Public Address: " + bitcoinActorAKeys.PublicAddress);
-        Console.WriteLine("Client A's Private Key: " + bitcoinActorAKeys.PrivateKey);
+        var signingActorAKeys = new SigningKeys();
+        Console.WriteLine("Client A's Public Address: " + signingActorAKeys.PublicAddress);
+        Console.WriteLine("Client A's Private Key: " + signingActorAKeys.PrivateKey);
 
         Console.WriteLine();
 
-        var bitcoinActorBKeys = new SigningKeys();
-        Console.WriteLine("Client B's Public Address: " + bitcoinActorBKeys.PublicAddress);
-        Console.WriteLine("Client B's Private Key: " + bitcoinActorBKeys.PrivateKey);
+        var signingActorBKeys = new SigningKeys();
+        Console.WriteLine("Client B's Public Address: " + signingActorBKeys.PublicAddress);
+        Console.WriteLine("Client B's Private Key: " + signingActorBKeys.PrivateKey);
 
         Console.WriteLine();
+
+        var messageToSign = "Hello ActorB";
+        // Sign the message
+        string signature = SigningKeys.SignMessage(messageToSign, signingActorAKeys.PrivateKey);
+
+        // Verify the signature
+        bool isVerified = SigningKeys.VerifySignature(messageToSign, signature, signingActorAKeys.PublicAddress);
+
+        Console.WriteLine($"Message: {messageToSign}");
+        Console.WriteLine($"Signature: {signature}");
+        Console.WriteLine($"Signature verified: {isVerified}");
+
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+
+
 
         // Encrypt Example
-        // var rsaActorAKeys = new EncryptKeys();
-        // Console.WriteLine("Hash: " + rsaActorAKeys.GetHashCode());
-        // Console.WriteLine("Client A's Public Key: " + rsaActorAKeys.PublicKey);
-        // Console.WriteLine();
-        // Console.WriteLine("Client A's Private Key: " + rsaActorAKeys.PrivateKey);
+        var rsaActorAKeys = new EncryptKeys();
+        Console.WriteLine("Hash: " + rsaActorAKeys.GetHashCode());
+        Console.WriteLine("Client A's Public Key: " + rsaActorAKeys.PublicKey);
+        Console.WriteLine();
+        Console.WriteLine("Client A's Private Key: " + rsaActorAKeys.PrivateKey);
         
-        // Console.WriteLine();
-        // Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
 
-        // var rsaActorBKeys = new EncryptKeys();
-        // Console.WriteLine("Hash: " + rsaActorBKeys.GetHashCode());
-        // Console.WriteLine("Client B's Public Key: " + rsaActorBKeys.PublicKey);
-        // Console.WriteLine();
-        // Console.WriteLine("Client B's Private Key: " + rsaActorBKeys.PrivateKey);
-        // Console.WriteLine();
-        // Console.WriteLine();
+        var rsaActorBKeys = new EncryptKeys();
+        Console.WriteLine("Hash: " + rsaActorBKeys.GetHashCode());
+        Console.WriteLine("Client B's Public Key: " + rsaActorBKeys.PublicKey);
+        Console.WriteLine();
+        Console.WriteLine("Client B's Private Key: " + rsaActorBKeys.PrivateKey);
+        Console.WriteLine();
+        Console.WriteLine();
 
-        // string message = "Hello Actor B";
-        // var encryptedMessage = EncryptKeys.Encrypt(message, rsaActorBKeys.PublicKey);
-        // Console.WriteLine("Message encrypted by Actor B public key: " + encryptedMessage);
-        // Console.WriteLine();
+        string messageToEncrypt = "Hello Actor B";
+        var encryptedMessage = EncryptKeys.Encrypt(messageToEncrypt, rsaActorBKeys.PublicKey);
+        Console.WriteLine("Message encrypted by Actor B public key: " + encryptedMessage);
+        Console.WriteLine();
 
-        // var messageDecryptedByActorB = EncryptKeys.Decrypt(encryptedMessage, rsaActorBKeys.PrivateKey);
-        // Console.WriteLine("Message decrypted by ActorB private key: " + messageDecryptedByActorB);
+        var messageDecryptedByActorB = EncryptKeys.Decrypt(encryptedMessage, rsaActorBKeys.PrivateKey);
+        Console.WriteLine("Message decrypted by ActorB private key: " + messageDecryptedByActorB);
     }
 }
 
